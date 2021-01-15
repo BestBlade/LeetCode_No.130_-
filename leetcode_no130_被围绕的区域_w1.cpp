b50866762ccdb9a7803cf9631a65bbd49120f5e2
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-01-15 10:17:01 +0800

    Add files via upload

---
+++ leetcode_no130_被围绕的区域_w1.cpp
@@ -0,0 +1,139 @@
+﻿/* ------------------------------------------------------------------|
+给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。
+
+找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+
+示例:
+
+X X X X
+X O O X
+X X O X
+X O X X
+运行你的函数后，矩阵变为：
+
+X X X X
+X X X X
+X X X X
+X O X X
+解释:
+
+被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/surrounded-regions
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	种子填充法/迭代法
+*
+*	执行用时：8 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：9.7 MB, 在所有 C++ 提交中击败了99.88%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+
+void solve(vector<vector<char>>& board) {
+    if (!board.size() || !board[0].size()) {
+        return;
+    }
+    queue<pair<int, int>> q;
+    int n = board.size();
+    int m = board[0].size();
+    for (int i = 0; i < m; i++) {
+        if (board[0][i] == 'O') {
+            q.push({ 0,i });
+            board[0][i] = 'A';
+        }
+        if (board[n - 1][i] == 'O') {
+            q.push({ n - 1,i });
+            board[n - 1][i] = 'A';
+        }
+    }
+    for (int i = 1; i < n - 1; i++) {
+        if (board[i][0] == 'O') {
+            q.push({ i,0 });
+            board[i][0] = 'A';
+        }
+        if (board[i][m - 1] == 'O') {
+            q.push({ i,m - 1 });
+            board[i][m - 1] = 'A';
+        }
+    }
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        if (x + 1 < n && board[x + 1][y] == 'O') {
+            board[x + 1][y] = 'A';
+            q.push({ x + 1,y });
+        }
+        if (x - 1 >= 0 && board[x - 1][y] == 'O') {
+            board[x - 1][y] = 'A';
+            q.push({ x - 1,y });
+        }
+        if (y + 1 < m && board[x][y + 1] == 'O') {
+            board[x][y + 1] = 'A';
+            q.push({ x,y + 1 });
+        }
+        if (y - 1 >= 0 && board[x][y - 1] == 'O') {
+            board[x][y - 1] = 'A';
+            q.push({ x,y - 1 });
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 'A') {
+                board[i][j] = 'O';
+            }
+            else if(board[i][j] == 'O') {
+                board[i][j] = 'X';
+            }
+        }
+    }
+    return;
+}
+
+int main() {
+    vector<vector<char>> board = { {'X','O','X','O','X','O'},{'O','X','O','X','O','X'},{'X','O','X','O','X','O'},{'O','X','O','X','O','X'} };
+    solve(board);
+}
---
+++ leetcode_no130_被围绕的区域_w2.cpp
@@ -0,0 +1,107 @@
+﻿/* ------------------------------------------------------------------|
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+注意:
+不能使用代码库中的排序函数来解决这道题。
+
+示例:
+
+输入: [2,0,2,1,1,0]
+输出: [0,0,1,1,2,2]
+进阶：
+
+一个直观的解决方案是使用计数排序的两趟扫描算法。
+首先，迭代计算出0、1 和 2 元素的个数，然后按照0、1、2的排序，重写当前数组。
+你能想出一个仅使用常数空间的一趟扫描算法吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-colors
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	递归法
+*
+*	执行用时：12 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：9.7 MB, 在所有 C++ 提交中击败了99.96%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+}; 
+int n, m;
+
+void dfs(vector<vector<char>>& board, int i, int j) {
+    if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != 'O') {
+        return;
+     }
+    board[i][j] = 'A';
+    dfs(board, i - 1, j);
+    dfs(board, i + 1, j);
+    dfs(board, i, j - 1);
+    dfs(board, i, j + 1);
+}
+
+void solve(vector<vector<char>>& board) {
+    n = board.size();
+    if (!n) {
+        return;
+    }
+    m = board[0].size();
+    for (int i = 0; i < m; i++) {
+        dfs(board, 0, i);
+        dfs(board, n - 1, i);
+    }
+    for (int i = 1; i < n - 1; i++) {
+        dfs(board, i, 0);
+        dfs(board, i, m - 1);
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 'A') {
+                board[i][j] = 'O';
+            }
+            else if (board[i][j] == 'O') {
+                board[i][j] = 'X';
+            }
+        }
+    }
+    return;
+}
